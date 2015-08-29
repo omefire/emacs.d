179156074ea04e34b5c3723ef7b578be21721f02
Omar Mefire <omefire@gmail.com>
2015-08-29 14:00:00 +0100

    GHC-MOD Instal

--- init.el
+++ init.el
@@ -27,12 +27,26 @@
 ;; (setq custom-file (locate-user-emacs-file "custom.el"))
 ;; (load custom-file 'noerror)
 
-;; HASKELL MODE
-;;(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+;; ============== HASKELL MODE ==============================
+
+;; Install ghc-mod first : https://github.com/omefire/emacs-haskell-tutorial/blob/master/tutorial.md#ghc-mod
+(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+
+(let ((my-cabal-path (expand-file-name "~/.cabal/bin")))
+  (setenv "PATH" (concat my-cabal-path ":" (getenv "PATH")))
+  (add-to-list 'exec-path my-cabal-path))
+
+;; package-install elpa package 'ghc'
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 ;;(put 'upcase-region 'disabled nil)
+
+;; ============== END OF HASKELL MODE ==============================
 
 (require 'package)
 (add-to-list 'package-archives
@@ -69,10 +83,10 @@
 (eval-after-load 'css-mode
   '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
 
-(eval-after-load 'js2-mode
-  '(add-hook 'js2-mode-hook
-             (lambda ()
-               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+;; (eval-after-load 'js2-mode
+;;   '(add-hook 'js2-mode-hook
+;;              (lambda ()
+;;                (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
 
 ;; Set tern up for javascript: See http://ternjs.net/doc/manual.html#emacs
 ;; ToDO: automate the install of all of this init.el on new machines (using make or similar tools). e.g: install npm if not installed and call it
@@ -91,16 +105,11 @@
 
 (put 'narrow-to-region 'disabled nil)
 
-;; NeoTree 
-(add-to-list 'load-path "elpa/neotree-0.2.1")
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
-(neotree-show)
 
 ;; ---------------------------------------
 ;; load elscreen (for tabbed windows)
 ;; ---------------------------------------
-(add-to-list 'load-path "elscreen")
+(add-to-list 'load-path "~/.emacs.d/elscreen")
 (load "elscreen.el")
 (elscreen-start)
 ;; F9 creates a new elscreen, F10 kills it
@@ -108,7 +117,15 @@
 (global-set-key [f10] 'elscreen-kill)
 
 ;; Enable ido-mode
-(ido-mode)
+(require 'ido)
+(ido-mode t)
+
+
+;; NeoTree 
+(add-to-list 'load-path "elpa/neotree-0.2.1")
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+(neotree-show)
 
 
 ;;-------------------------------------------------------
